9f8e1f3ded5183ae3b0907c1ec389c8b4d8ee985
Sam Bond-Taylor <samb-t@hotmail.co.uk>
2020-10-02 14:29:26 +0100

    changed nonlinearity to ELU

--- GON.py
+++ GON.py
@@ -26,16 +26,13 @@ class Generator(nn.Module):
         self.main = nn.Sequential(
             nn.ConvTranspose2d(nz, ngf * 4, 4, 1, 0, bias=True),
             nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
-
+            nn.ELU(),
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
-
+            nn.ELU(),
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
-
+            nn.ELU(),
             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=True),
             nn.Sigmoid()
         )
